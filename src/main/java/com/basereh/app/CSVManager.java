19516ecc1b529f5647431e7edd181ca60a6c7118
Reza Basereh <basereh@mahsan.net>
2023-09-05 14:35:21 +0330

    feat: print csv parser options in cli and their functionalities

--- src/main/java/com/basereh/app/CSVManager.java
+++ src/main/java/com/basereh/app/CSVManager.java
@@ -2,20 +2,36 @@ package com.basereh.app;
 
 import java.lang.String;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class CSVManager {
 
-    public String[][] parser(String formatedString) {
-        String[] parsedLines = formatedString.split("\\n");
-        return parser(parsedLines);
+//    public static String read() {
+//        Scanner scanner = new Scanner(System.in);
+//        String result = scanner.next();
+//        scanner.close();
+//        return result;
+//    }
+
+//    public String[] readLineByLine() {
+//        System.out.println("press ESC key to finish");
+//        Scanner scanner = new Scanner(System.in);
+//        ArrayList<String> result = new ArrayList<>();
+//        while (scanner.hasNext()) {
+//            result.add(scanner.next());
+//        }
+//        scanner.close();
+//        return result.toArray(String[]::new);
+//    }
+
+    public static String[][] parser(String formatedString) {
+        String[] parsedLines = formatedString.split(";");
+        return Arrays.stream(parsedLines).map(line -> line.split(",")).toArray(String[][]::new);
     }
 
-    public String[][] parser(String[] formatedLines) {
-        return Arrays.stream(formatedLines).map(line -> line.split(",")).toArray(String[][]::new);
-    }
-
-    public void print(String[][] parsedRows) {
+    public static void print(String[][] parsedRows) {
         int[] maxLengths = new int[parsedRows[0].length];
 
         for (int i = 0; i < parsedRows.length; i++) {
@@ -31,7 +47,7 @@ public class CSVManager {
         String format = formatBuilder.toString();
 
         StringBuilder tableBuilder = new StringBuilder();
-        for (String[] row: parsedRows){
+        for (String[] row : parsedRows) {
             tableBuilder.append(String.format(format, row)).append("\n");
         }
         System.out.println(tableBuilder);
