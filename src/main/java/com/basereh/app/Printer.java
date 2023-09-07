cfa4475bd45ece0c52f0b80f9bb340daa095851b
Reza Basereh <basereh@mahsan.net>
2023-09-07 13:30:49 +0330

    feat: add print method to Printer to show statistics results

--- src/main/java/com/basereh/app/Printer.java
+++ src/main/java/com/basereh/app/Printer.java
@@ -1,7 +1,7 @@
 package com.basereh.app;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Printer {
     public void print(CSV csv) {
@@ -16,8 +16,8 @@ public class Printer {
             }
         }
         StringBuilder formatBuilder = new StringBuilder();
-        for (int i = 0; i < maxLengths.length; i++) {
-            formatBuilder.append("|%-").append(maxLengths[i] + 2).append("s");
+        for (int maxLength : maxLengths) {
+            formatBuilder.append("|%-").append(maxLength + 2).append("s");
         }
         String format = formatBuilder.toString();
 
@@ -26,5 +26,18 @@ public class Printer {
             tableBuilder.append(String.format(format, record.toArray())).append("\n");
         }
         System.out.println(tableBuilder);
+    }
+
+    public void print(List<StatisticsResult> results) {
+        Map<StatisticTarget, List<StatisticsResult>> groupedResults = results.stream().collect(Collectors.groupingBy(StatisticsResult::getTarget));
+        groupedResults.forEach((statisticTarget, statisticsResults) -> {
+            System.out.println("\n" + statisticTarget);
+            statisticsResults.forEach(result -> {
+                System.out.println("\t" + result.getName());
+                result.getMeasures().forEach(measure -> {
+                    System.out.println("\t\t" + measure.getName() + ":" + measure.getValue());
+                });
+            });
+        });
     }
 }
